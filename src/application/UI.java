2f09304e5d3de1727edb1f82b39987dbbcc49b34
UitaloAndrade <uitalo.a.reis@gmail.com>
2023-07-07 20:16:27 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -56,6 +55,10 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
